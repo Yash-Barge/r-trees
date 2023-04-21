bcf9cf2158259e652c4e8eeb4ca61e89de6a270e
Yash Barge <yhbarge@gmail.com>
2023-04-21 20:36:03 +0530

    changed structure of directory to match that specified by sir

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include "hello.h"
-
-void hello_world(void) {
-    printf("Hello World!\n");
-    
-    return;
-}
--- main.c
+++
@@ -1,7 +0,0 @@
-#include "hello.h"
-
-int main(void) {
-    hello_world();
-
-    return 0;
-}
